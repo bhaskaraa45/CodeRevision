b7d98d312c60fb1f8471d947cd148eabc4370058
bhaskaraa45 <bhaskarmandal369@gmail.com>
2023-06-19 23:44:45 +0530

    bottom navigation

--- app/src/main/java/com/android/aa45/coderevision/MainActivity.java
+++ app/src/main/java/com/android/aa45/coderevision/MainActivity.java
@@ -1,40 +1,53 @@
 package com.android.aa45.coderevision;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.android.aa45.coderevision.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_bottom_nev_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if(id==R.id.home) {
-            Toast.makeText(getApplicationContext(), "home Selected", Toast.LENGTH_LONG).show();
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+            int id = item.getItemId();
+            int home = R.id.home;
+            int me = R.id.me;
+            int search = R.id.search;
+
+            if(id==home){
+                replaceFragment(new HomeFragment());
+            } else if (id==search) {
+                replaceFragment(new SearchFragment());
+            } else if (id==me) {
+                replaceFragment(new MeFragment());
+            }
             return true;
-        }else if(id==R.id.search) {
-            Toast.makeText(getApplicationContext(), "search Selected", Toast.LENGTH_LONG).show();
-            return true;
-        }else if(id== R.id.me){
-            Toast.makeText(getApplicationContext(), "me Selected", Toast.LENGTH_LONG).show();
-            return true;
-        }else {
-            return super.onOptionsItemSelected(item);
-        }
-        }
+        });
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        @SuppressLint("CommitTransaction") FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
+}
