afa12b017f45cc74c359a872d5ba4be7eca23b13
bhaskaraa45 <bhaskarmandal369@gmail.com>
2023-06-21 16:59:23 +0530

    dialog added for floating addButton

--- app/src/main/java/com/android/aa45/coderevision/HomeFragment.java
+++ app/src/main/java/com/android/aa45/coderevision/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.android.aa45.coderevision;
 
 
-import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,9 +12,13 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.android.aa45.coderevision.Adapters.FragPageAdapter;
 import com.android.aa45.coderevision.Fragments.SolvedFragment;
@@ -22,9 +28,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 
-public class HomeFragment extends Fragment {
-
-    private FloatingActionButton addButton;
+public class HomeFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,23 +41,80 @@ public class HomeFragment extends Fragment {
         ViewPager vp = view.findViewById(R.id.viewPager);
         tl.setupWithViewPager(vp);
         replaceFragment(new SolvedFragment());
-        FragPageAdapter adapter = new FragPageAdapter(requireActivity().getSupportFragmentManager(),FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        FragPageAdapter adapter = new FragPageAdapter(requireActivity().getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         adapter.fragmentAdd(new SolvedFragment(), "Solved");
         adapter.fragmentAdd(new TriedFragment(), "Tried");
         adapter.fragmentAdd(new WishlistFragment(), "Wishlist");
         vp.setAdapter(adapter);
 
-        addButton = view.findViewById(R.id.addButton);
 
+        FloatingActionButton addButton = view.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
 
-        return view;
+       return view;
     }
+
+
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-        @SuppressLint("CommitTransaction") FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setReorderingAllowed(true);
         fragmentTransaction.replace(R.id.viewPager,fragment);
         fragmentTransaction.commit();
     }
 
+    public void showDialog(){
+        final Dialog dialog = new Dialog(getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_add_button);
+
+        LinearLayout addSolved = dialog.findViewById(R.id.addSolved);
+        LinearLayout addTried = dialog.findViewById(R.id.addTried);
+        LinearLayout addWishlist = dialog.findViewById(R.id.addWishlist);
+        addSolved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(getContext(),"solved is Clicked",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        addTried.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(getContext(),"tried is Clicked",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        addWishlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(getContext(),"wishlist is Clicked",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.FloatingActionButton;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+
+
+    }
 
 }
